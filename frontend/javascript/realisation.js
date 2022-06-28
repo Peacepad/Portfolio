01bed5bc629485b269499eb60b6163cf5a8b0cb2
Peace <padelamare@gmail.com>
2022-06-28 14:15:01 +0200

    perf: optimize image size and first contentful paint
    update image format and delete police doesn't use

--- frontend/javascript/realisation.js
+++ frontend/javascript/realisation.js
@@ -1,44 +1,46 @@
 async function realisationsMain() {
   const realisationsList = document.querySelector(".realisations-list");
 
-
   let realisations = [
     {
-      "name": "Groupomania",
-      "description": "Le projet consiste à construire un réseau social interne pour les employés de Groupomania. Le but de cet outil est de faciliter les interactions entre collègues. Le département RH de Groupomania a laissé libre cours à son imagination pour les fonctionnalités du réseau et a imaginé plusieurs briques pour favoriser les échanges entre collègues.",
-      "image": "./frontend/public/images/groupomania.png",
-      "link": "https://github.com/Peacepad/groupomania"
+      name: "Groupomania",
+      description:
+        "Le projet consiste à construire un réseau social interne pour les employés de Groupomania. Le but de cet outil est de faciliter les interactions entre collègues. Le département RH de Groupomania a laissé libre cours à son imagination pour les fonctionnalités du réseau et a imaginé plusieurs briques pour favoriser les échanges entre collègues.",
+      image: "./frontend/public/images/groupomania.webp",
+      link: "https://github.com/Peacepad/groupomania",
     },
     {
-      "name": "OhMYFood",
-      "description": "L'objectif d'intégrer puis de dynamiser une page web avec des animations CSS en utilisant le préprocesseur Sass.",
-      "image": " ./frontend/public/images/ohmyfood.png",
-      "link": "https://github.com/Peacepad/ohmyfood"
+      name: "OhMYFood",
+      description:
+        "L'objectif d'intégrer puis de dynamiser une page web avec des animations CSS en utilisant le préprocesseur Sass.",
+      image: " ./frontend/public/images/ohmyfood.webp",
+      link: "https://github.com/Peacepad/ohmyfood",
     },
     {
-      "name":"La chouette Agence",
-      "description":"Optimiser le référencement SEO d'un site web existant, tout en améliorant l'accessibilité du site, les performances.",
-      "image": " ./frontend/public/images/lachouetteagence.png",
-      "link": "https://github.com/Peacepad/lachouetteagence"
+      name: "La chouette Agence",
+      description:
+        "Optimiser le référencement SEO d'un site web existant, tout en améliorant l'accessibilité du site, les performances.",
+      image: " ./frontend/public/images/lachouetteagence.webp",
+      link: "https://github.com/Peacepad/lachouetteagence",
     },
     {
-      "name":"Piquante",
-      "description":"Le front-end de l'application a été développé à l'aide d'Angular mais Piiquante a besoin d'un développeur pour construire l'API.",
-      "image": "./frontend/public/images/piquante.png",
-      "link": "https://github.com/Peacepad/piquante"
+      name: "Piquante",
+      description:
+        "Le front-end de l'application a été développé à l'aide d'Angular mais Piiquante a besoin d'un développeur pour construire l'API.",
+      image: "./frontend/public/images/piquante.webp",
+      link: "https://github.com/Peacepad/piquante",
     },
     {
-      "name":"Orinoco",
-      "description":"Création du front-end d'un site e-commerce en Javascript en utilisant un API fourni.",
-      "image": "./frontend/public/images/orinoco.png",
-      "link": "https://github.com/Peacepad/orinoco"
-    }
-
+      name: "Orinoco",
+      description:
+        "Création du front-end d'un site e-commerce en Javascript en utilisant un API fourni.",
+      image: "./frontend/public/images/orinoco.webp",
+      link: "https://github.com/Peacepad/orinoco",
+    },
   ];
 
   // async function getRealisations() {
 
-    
   //   return fetch("http://localhost:8000/api/projet")
   //     .then((res) => {
   //       return res.json();
@@ -58,7 +60,7 @@ async function realisationsMain() {
       // Création d'une div par realisation
       let realisationDiv = document.createElement("div");
       realisationDiv.setAttribute("class", "realisation appear-scroll");
-      
+
       realisationDiv.setAttribute("id", `realisation__${l + 1}`);
       realisationsList.appendChild(realisationDiv);
 
@@ -74,9 +76,7 @@ async function realisationsMain() {
   const realisationDescription = document.querySelector(
     "#realisation-description"
   );
-  const realisationLetter = document.querySelector(
-    "#realisation-letter"
-  );
+  const realisationLetter = document.querySelector("#realisation-letter");
   // Ajout du titre, description au survol de l'élément
   async function realisationHover() {
     for (let i = 0; i < realisationsDOM.length; i++) {
