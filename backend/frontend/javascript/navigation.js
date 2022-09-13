bcfa52df127579f028d11bcbeb76d94079f316c0
Peace <padelamare@gmail.com>
2022-09-14 00:41:35 +0200

    style: adjust height and navigation
    same style for navigation in creations pages

--- backend/frontend/javascript/navigation.js
+++ backend/frontend/javascript/navigation.js
@@ -1,27 +1,35 @@
 const navBtn = document.querySelector(".nav-btn");
 const navMobile = document.querySelector(".navigation-mobile");
+
 let navHidden = true;
 navBtn.addEventListener("click", handleNavBtn);
 
 function handleNavBtn() {
   if (navHidden) {
-    navMobile.style.transform = "translateY(0%)";
+    navMobile.style.transform = "translate(-50%,-50%)";
     navHidden = false;
     navBtn.style.opacity = "0";
-    setTimeout(
-      `navBtn.innerHTML = '<img src="./public/images/icons/close.svg" alt="close" />'`,
+    setTimeout(() => {
+      navBtn.innerHTML = '<img src="./public/images/icons/close.svg" alt="close" />'},
       300
     );
-    setTimeout(`navBtn.style.opacity ="1"`, 300);
+    setTimeout(() => {
+      navBtn.style.opacity ="1";
+     
+    }, 300);
   } else {
-    navMobile.style.transform = "translateY(-100%)";
+    navMobile.style.transform = "translate(-50%,-200%)";
     navHidden = true;
     navBtn.style.opacity = "0";
-    setTimeout(
-      `navBtn.innerHTML = '<img src="./public/images/icons/menu.svg" alt="menu" />'`,
+    setTimeout(() => {
+      navBtn.innerHTML = '<img src="./public/images/icons/menu.svg" alt="menu" />'},
       300
     );
-    setTimeout(`navBtn.style.opacity ="1"`, 300);
+    setTimeout(
+      () => {
+        navBtn.style.opacity ="1";
+       
+      }, 300);
   }
 }
 
